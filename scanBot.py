9af79e336f64a588226b18e2d3db0843ad364f9b
Vasily Khodyrev <vasily.khodyrev@devexperts.com>
2023-08-09 18:22:51 +0300

    Add protection for scans - only owner can initiate new scan

--- scanBot.py
+++ scanBot.py
@@ -8,6 +8,7 @@ import telebot
 import subprocess
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+OWNER_TELE_ID = int(os.environ.get('OWNER_TELE_ID'))
 
 script_is_running = False
 
@@ -21,38 +22,46 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['scan_stock'])
 def scan_and_send(message: telebot.types.Message):
     start = time.time()
-    global script_is_running
-    if not script_is_running:
-        script_is_running = True
-        print(f"starting scanner script")
-        command = ['cmd', '/c', 'python', 'scanner.py']
-        process = subprocess.Popen(command)
-        process.wait()
-        end = time.time()
-        total_time = end - start
-        print(f"Script completed with return code {process.returncode} in {str(total_time)} sec")
-        script_is_running = False
+    print(f"scan_stock from user {message.from_user.id}")
+    if message.from_user.id == OWNER_TELE_ID:
+        global script_is_running
+        if not script_is_running:
+            script_is_running = True
+            print(f"starting scanner script")
+            command = ['cmd', '/c', 'python', 'scanner.py']
+            process = subprocess.Popen(command)
+            process.wait()
+            end = time.time()
+            total_time = end - start
+            print(f"Script completed with return code {process.returncode} in {str(total_time)} sec")
+            script_is_running = False
+        else:
+            print(f"Script is already running - skip.")
     else:
-        print(f"Script is already running - skip.")
+        bot.reply_to(message, "Sorry - you're not owner of this bot - you can't initiate scan")
     send_last_stock_result(message)
 
 
 @bot.message_handler(commands=['scan_futures'])
 def scan_and_send(message: telebot.types.Message):
     start = time.time()
-    global script_is_running
-    if not script_is_running:
-        script_is_running = True
-        print(f"starting scanner script")
-        command = ['cmd', '/c', 'python', 'scanner.py', 'futures']
-        process = subprocess.Popen(command)
-        process.wait()
-        end = time.time()
-        total_time = end - start
-        print(f"Script completed with return code {process.returncode} in {str(total_time)} sec")
-        script_is_running = False
+    print(f"scan_futures from user {message.from_user.id}")
+    if message.from_user.id == OWNER_TELE_ID:
+        global script_is_running
+        if not script_is_running:
+            script_is_running = True
+            print(f"starting scanner script")
+            command = ['cmd', '/c', 'python', 'scanner.py', 'futures']
+            process = subprocess.Popen(command)
+            process.wait()
+            end = time.time()
+            total_time = end - start
+            print(f"Script completed with return code {process.returncode} in {str(total_time)} sec")
+            script_is_running = False
+        else:
+            print(f"Script is already running - skip.")
     else:
-        print(f"Script is already running - skip.")
+        bot.reply_to(message, "Sorry - you're not owner of this bot - you can't initiate scan")
     send_last_futures_result(message)
 
 
